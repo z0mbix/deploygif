21ce53c968b4eb0b16977d976a915ccc1af9eecf
z0mbix <zombie@zombix.org>
2016-05-19 16:43:53 +0100

    Add support for .json. Throw a 503 when redis is unavailable.

--- app.lua
+++ app.lua
@@ -1,5 +1,7 @@
 -- This is the regex match ($1) from the location regex (success|fail)
 local route = ngx.var[1]
+-- This is the regex match ($2) from the location regex (/|.json)
+local dot_json = ngx.var[2]
 local redirect = ngx.var.arg_redirect
 local json = ngx.var.arg_json
 local cjson = require "cjson"
@@ -11,28 +13,39 @@ ngx.header["content-type"] = "text/plain"
 
 red:set_timeout(1000) -- 1 sec
 
+-- TODO: How to load settings from a config file?
 local ok, err = red:connect("127.0.0.1", 6379)
 if not ok then
-    ngx.redirect("/sorry", 302)
+    #ngx.redirect("/sorry", 302)
+    ngx.status = 503
+    ngx.say("I'm terribly sorry, I messed something up :(")
+    ngx.exit(ngx.OK)
 end
 
 local res, err = red:srandmember(route)
 
 if not res then
-    ngx.say("failed to get gif: ", err)
+    ngx.say("Failed to find you a gif: ", err)
     return
 end
 
+-- Nothing back from redis?
 if res == ngx.null then
     ngx.say("Sorry, no gifs found.")
     return
 end
 
+-- Did the request end with .json?
+if dot_json == '.json' then
+    json = '1'
+end
+
+-- Did the request ask for json?
 if accept_header == 'application/json' then
     json = '1'
 end
 
--- put it into the connection pool of size 100,
+-- Put it into the connection pool of size 100,
 -- with 10 seconds max idle time
 local ok, err = red:set_keepalive(10000, 100)
 if not ok then
@@ -53,3 +66,4 @@ else
 end
 
 red:close()
+ngx.exit(ngx.OK)
